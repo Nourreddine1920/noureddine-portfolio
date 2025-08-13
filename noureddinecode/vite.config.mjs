c640993a085dc4127fd22a7f446b5b5f3094d39c
Nourreddine1920 <noureddine.awledbrahim@gmail.com>
2025-08-13 14:48:09 +0100

    feat: update the vite file

--- noureddinecode/vite.config.mjs
+++ noureddinecode/vite.config.mjs
@@ -3,14 +3,11 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tagger from "@dhiwise/component-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // This changes the out put dir from dist to build
-  // comment this out if that isn't relevant for your project
+  base: '/noureddine-portfolio/', // <-- move here!
   build: {
     outDir: "build",
     chunkSizeWarningLimit: 2000,
-    base: '/noureddine-portfolio/', // <-- set this to your repo name
   },
   plugins: [tsconfigPaths(), react(), tagger()],
   server: {
